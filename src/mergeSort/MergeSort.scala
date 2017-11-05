d0d37f9a67dafb5af80eeb67eeccf7062b88b042
Casper12345 <caspertellerup@hotmail.com>
2017-11-05 16:01:35 +0000

    merge sort

---
+++ src/mergeSort/MergeSort.scala
@@ -0,0 +1,36 @@
+package mergeSort
+
+object MergeSort {
+  def apply() = new MergeSort
+}
+
+class MergeSort {
+
+  def sort[A <: Ordered[A]](lst: List[A]): List[A] = {
+    val m = lst.size / 2
+
+    val split = lst.splitAt(m)
+
+    sort(split._2)
+    sort(split._1)
+
+
+    Nil
+  }
+
+  def merge[A <: Ordered[A]](lst1: List[A], lst2: List[A], lst: List[A]): List[A] = {
+    def looper(lst: List[A], i: Int = lst.size): A = i match {
+      case 0 =>
+
+    }
+
+  }
+
+
+}
+
+object Main extends App {
+  val m = MergeSort.apply()
+  m.sort(List(1, 2, 3, 4, 5, 6))
+
+}
