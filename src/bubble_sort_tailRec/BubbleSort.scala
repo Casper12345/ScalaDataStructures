6d03edeac97bb4d054714dedef7d224c43f8c84d
Casper12345 <caspertellerup@hotmail.com>
2019-01-27 14:32:06 +0000

    b sort tailRec added

---
+++ src/bubble_sort_tailRec/BubbleSort.scala
@@ -0,0 +1,32 @@
+package bubble_sort_tailRec
+
+import scala.annotation.tailrec
+
+object BubbleSort extends App {
+
+  @tailrec
+  def bubbleSort[A](xs: List[A])(f: (A, A) => Boolean): List[A] = {
+    @tailrec
+    def helper(xs: List[A], acc: List[A], isSorted: Boolean): (List[A], Boolean) = xs match {
+      case Nil => (acc, isSorted)
+      case h :: Nil => helper(Nil, acc :+ h, isSorted)
+      case h :: t =>
+        if (f(h, t.head)) {
+          helper(h :: t.tail, acc :+ t.head, isSorted = true)
+        } else {
+          helper(t, acc :+ h, isSorted)
+        }
+    }
+
+    val (l, b) = helper(xs, Nil, isSorted = false)
+    if (b) {
+      bubbleSort(l)(f)
+    } else {
+      l
+    }
+
+  }
+
+  println(bubbleSort(List(1, 2, 3, 4, 5, 6, 7, 8).reverse)((a: Int, b: Int) => a > b))
+
+}
