07c7007b2bc252c8457fcccb917009fdd893c3c9
Casper Nielsen <caspertellerup@outlook.com>
2022-03-10 16:08:50 +0100

    contains added

--- src/binary_search_tree/Main.scala
+++ src/binary_search_tree/Main.scala
@@ -1,11 +1,13 @@
 package binary_search_tree
 
-sealed trait Tree
-case class Branch[A](l: Tree, r: Tree, v: A) extends Tree
-case object Leaf extends Tree
+import scala.annotation.tailrec
+
+sealed trait Tree[+A]
+case class Branch[A](l: Tree[A], r: Tree[A], v: A) extends Tree[A]
+case object Leaf extends Tree[Nothing]
 
 object Tree extends App {
-  def fromList[A](xs: List[A], f:(A,A) => Boolean): Tree = {
+  def fromList[A](xs: List[A], f:(A,A) => Boolean): Tree[A] = {
     def split(xs: List[A]): (A, List[A], List[A]) = {
       val (left, r) = xs.splitAt(xs.length / 2)
       val root = r.head
@@ -13,7 +15,7 @@ object Tree extends App {
       (root, left, right)
     }
 
-    def go(xs: List[A]): Tree = xs.length match {
+    def go(xs: List[A]): Tree[A] = xs.length match {
       case 3 =>
         val (root, left, right) = split(xs)
         Branch(Branch(Leaf, Leaf, left.head), Branch(Leaf, Leaf, right.head), root)
@@ -32,7 +34,34 @@ object Tree extends App {
 
     go(xs.sortWith(f).distinct)
   }
-      val xs = List("b","a","c","e","d")
-      println(Tree.fromList[String](xs, _ < _))
+
+  def insert[A](t: Tree[A], a: A, f: (A,A) => Boolean): Tree[A] = {
+    def go(t: Tree[A]): Tree[A] = t match {
+      case Branch(l: Branch[A], r: Branch[A], v) => if(f(a,v)) Branch(go(l), r, v) else Branch(l, go(r), v)
+      case Branch(l: Branch[A], Leaf, v) => if(f(a,v)) Branch(go(l), Leaf, v) else Branch(l, Branch(Leaf, Leaf, a), v)
+      case Branch(Leaf, r: Branch[A], v) => if(f(a,v)) Branch(Leaf, Branch(Leaf, Leaf, a), v) else Branch(Leaf, go(r), v)
+      case Branch(Leaf, Leaf, v) => if(f(a,v)) Branch(Branch(Leaf, Leaf, a), Leaf, v) else Branch(Leaf, Branch(Leaf, Leaf, a), v)
+      case Leaf => Leaf
+    }
+    go(t)
+
+  }
+
+  def contains[A](t: Tree[A], a: A, f: (A,A) => Boolean): Boolean = {
+    @tailrec
+    def go(t: Tree[A]): Boolean = t match {
+      case Branch(_, _, v) if v == a => true
+      case Branch(l, r, v) if v != a => if(f(a,v)) go(l) else go(r)
+      case Leaf => false
+    }
+    go(t)
+  }
+
+  val xs = List(1,2,3,4,5,6)
+  val t = Tree.fromList[Int](xs, _ < _)
+  println(t)
+  val i = insert[Int](t, 6, _ <= _)
+  println(i)
+  println(contains[Int](i, 9, _ <= _))
 
 }
