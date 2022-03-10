5b280d0fc77e837048a4c1f379c87be2b3dffd18
Casper Nielsen <caspertellerup@outlook.com>
2022-03-10 14:59:42 +0100

    binary search tree

---
+++ src/binary_search_tree/Main.scala
@@ -0,0 +1,37 @@
+package binary_search_tree
+
+sealed trait Tree
+case class Branch[A](l: Tree, r: Tree, v: A) extends Tree
+case object Leaf extends Tree
+
+object Tree extends App {
+  def fromList[A](xs: List[A], f:(A,A) => Boolean): Tree = {
+    def split(xs: List[A]): (A, List[A], List[A]) = {
+      val (left, r) = xs.splitAt(xs.length / 2)
+      val root = r.head
+      val right = r.tail
+      (root, left, right)
+    }
+    def go(xs: List[A]): Tree = {
+      if (xs.length == 3) {
+        val (root, left, right) = split(xs)
+        Branch(Branch(Leaf, Leaf, left.head), Branch(Leaf, Leaf, right.head), root)
+      } else if (xs.length == 2) {
+        val (root, left, _) = split(xs)
+        Branch(Branch(Leaf, Leaf, left.head), Leaf, root)
+      } else if (xs.length == 1) {
+        val (root, _, _) = split(xs)
+        Branch(Leaf, Leaf, root)
+      } else if (xs.isEmpty) {
+        Leaf
+      } else {
+        val (root, left, right) = split(xs)
+        Branch(go(left), go(right), root)
+      }
+    }
+    go(xs.sortWith(f).distinct)
+  }
+      val xs = List("b","a","c","e","d")
+      println(Tree.fromList[String](xs, _ < _))
+
+}
