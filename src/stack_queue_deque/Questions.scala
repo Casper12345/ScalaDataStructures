19002c7410049b1b8c04c4ebe5a2d0b412507d8e
Casper Nielsen <caspertellerup@outlook.com>
2022-04-09 11:12:39 +0200

    queue added

--- src/stack_queue_deque/Questions.scala
+++ src/stack_queue_deque/Questions.scala
@@ -2,37 +2,56 @@ package stack_queue_deque
 
 import scala.annotation.tailrec
 
+object Queue {
+  sealed trait Queue[+A]
+  case class Element[A](a: A, q: Queue[A]) extends Queue[A]
+  case object End extends Queue[Nothing]
 
-sealed trait Stack[+A]
-case class Element[A](a: A, stack: Stack[A]) extends Stack[A]
-case object End extends Stack[Nothing]
+  def enqueue[A](a: A, q: Queue[A]): Queue[A] = Element(a, q)
 
-object Questions {
+  def dequeue[A](q: Queue[A]): Option[(A, Queue[A])] = {
+    var optA: Option[A] = None
+    def go(q: Queue[A]): Queue[A] = q match {
+      case End => End
+      case Element(a, End) => optA = Some(a); End
+      case Element(a, Element(e, End)) => optA = Some(e); Element(a, End)
+      case Element(a, q) => Element(a, go(q))
+    }
+    val res = go(q)
+    optA.map(a => (a, res))
 
-  def push[A](a: A, s: Stack[A]): Stack[A] = Element(a, s)
-
-  def pop[A](s: Stack[A]): Option[(A, Stack[A])] = s match {
-    case Element(a, stack) => Some((a, stack))
-    case End => None
   }
 
-  def top[A](s: Stack[A]): Option[A] = s match {
-    case Element(a, _) => Some(a)
-    case End => None
+}
+
+object Stack {
+  sealed trait Stack[+A]
+  case class Element[A](a: A, stack: Stack[A]) extends Stack[A]
+  case object End extends Stack[Nothing]
+
+  def push[A](a: A, s: Stack.Stack[A]): Stack.Stack[A] = Stack.Element(a, s)
+
+  def pop[A](s: Stack.Stack[A]): Option[(A, Stack.Stack[A])] = s match {
+    case Stack.Element(a, stack) => Some((a, stack))
+    case Stack.End => None
   }
 
-  def size[A](s: Stack[A]): Int = {
+  def top[A](s: Stack.Stack[A]): Option[A] = s match {
+    case Stack.Element(a, _) => Some(a)
+    case Stack.End => None
+  }
+
+  def size[A](s: Stack.Stack[A]): Int = {
     @tailrec
-    def go(count: Int, s: Stack[A]): Int = s match {
-      case Element(_, stack) => go(count + 1, stack)
-      case End => count
+    def go(count: Int, s: Stack.Stack[A]): Int = s match {
+      case Stack.Element(_, stack) => go(count + 1, stack)
+      case Stack.End => count
     }
     go(0, s)
   }
 
-  def isEmpty[A](s: Stack[A]): Boolean = s match {
-    case Element(_, _) => false
-    case End => true
+  def isEmpty[A](s: Stack.Stack[A]): Boolean = s match {
+    case Stack.Element(_, _) => false
+    case Stack.End => true
   }
-
 }
